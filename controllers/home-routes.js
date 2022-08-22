d86758976cd4416d1463bccacf3b2ec44dc51bf5
Sean Genge <seangenge@gmail.com>
2022-08-22 21:05:59 +1000

    Skeleton code

--- controllers/home-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const withAuth = require('../utils/auth');
-
-router.get('/', (req, res) => {
-  res.render('homepage');
-});
-
-module.exports = router;
